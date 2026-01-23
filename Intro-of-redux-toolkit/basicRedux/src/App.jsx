8b6e1a8ec4badcc0e25ea526e43438336229c99e
ez-sourav <souravb2003@gmail.com>
2026-01-23 19:27:36 +0530

    add useDispatch and useSelector

--- Intro-of-redux-toolkit/basicRedux/src/App.jsx
+++ Intro-of-redux-toolkit/basicRedux/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counterSlice";
 
 const App = () => {
+
+  const dispatch = useDispatch();
+  const count = useSelector((state)=>state.counter.value);
+
   return (
     <div>
-      <h1>0</h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h1>{count}</h1>
+      <button onClick={() => {dispatch(increment())}}>Increment</button>
+      <button onClick={() => {dispatch(decrement())}}>Decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
