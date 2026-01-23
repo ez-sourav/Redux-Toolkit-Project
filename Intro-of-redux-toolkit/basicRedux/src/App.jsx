8232047e72a106bac2647541cf4bc9b71f2a5c5d
ez-sourav <souravb2003@gmail.com>
2026-01-23 19:42:04 +0530

    commit

--- Intro-of-redux-toolkit/basicRedux/src/App.jsx
+++ Intro-of-redux-toolkit/basicRedux/src/App.jsx
@@ -1,17 +1,47 @@
-
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./features/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByAmmount,
+} from "./features/counterSlice";
+import { useState } from "react";
 
 const App = () => {
-
   const dispatch = useDispatch();
-  const count = useSelector((state)=>state.counter.value);
+  const count = useSelector((state) => state.counter.value);
+  const [num, setnum] = useState();
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => {dispatch(increment())}}>Increment</button>
-      <button onClick={() => {dispatch(decrement())}}>Decrement</button>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <input
+        type="number"
+        value={num}
+        onChange={(e) => {
+          setnum(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(incrementByAmmount(Number(num)));
+        }}
+      >
+        Increment By Ammount
+      </button>
     </div>
   );
 };
