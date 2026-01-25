1f534a06d33cdf054a7b5eb8d4e73d027d2e45dd
ez-sourav <souravb2003@gmail.com>
2026-01-25 18:10:00 +0530

    add videos api

--- Project/searchPhotos/src/App.jsx
+++ Project/searchPhotos/src/App.jsx
@@ -1,11 +1,19 @@
-import { fetchPhotos } from "./api/mediaApi"
+import { fetchPhotos, fetchVideos } from "./api/mediaApi"
 
 const App = () => {
   return (
     <div className='bg-gray-950 h-screen w-full text-white'>
-      <button onClick={()=>{
-        fetchPhotos('cat')
+      <button className="bg-amber-500 px-5 py-3 mr-5" onClick={ async ()=>{
+        const data = await fetchPhotos('dog')
+        console.log(data.results);
+        
       }}>Get Photos</button>
+
+      <button className="bg-amber-500 px-5 py-3 mr-5" onClick={ async ()=>{
+        const data = await fetchVideos('dog')
+        console.log(data.videos);
+        
+      }}>Get Videos</button>
     </div>
   )
 }
