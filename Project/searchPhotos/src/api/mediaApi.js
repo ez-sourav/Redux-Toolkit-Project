54b5ff86770d86baf0ec65750c59c9bdbd5f2469
ez-sourav <souravb2003@gmail.com>
2026-01-25 18:09:49 +0530

    add videos api

--- Project/searchPhotos/src/api/mediaApi.js
+++ Project/searchPhotos/src/api/mediaApi.js
@@ -2,11 +2,20 @@ import axios from "axios";
 
 const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+const GIPHY_KEY = import.meta.env.VITE_GIPHY_KEY
 
-export async function fetchPhotos(query,page=1,perpage=20){
+export async function fetchPhotos(query,page=1,per_page=20){
     const res =await axios.get('https://api.unsplash.com/search/photos',{
-        params:{query,page,perpage},
+        params:{query,page,per_page},
         headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
     })
-    console.log(res)
+    return res.data
+}
+
+export async function fetchVideos(query,per_page=15) {
+    const res =await axios.get('https://api.pexels.com/videos/search',{
+        params:{query,per_page},
+        headers:{Authorization:PEXELS_KEY}
+    })
+    return res.data
 }
