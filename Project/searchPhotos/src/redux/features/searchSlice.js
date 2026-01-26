ae0c3a7231b2f60d665339ee4eefbe798984ba42
ez-sourav <souravb2003@gmail.com>
2026-01-26 20:06:10 +0530

    search Slice

--- Project/searchPhotos/src/redux/features/searchSlice.js
+++ Project/searchPhotos/src/redux/features/searchSlice.js
@@ -18,16 +18,22 @@ const searchSlice = createSlice({
         },
         setResults(state, action) {
             state.results = action.payload
+            state.loading = false
         },
-        setLoading(state, action) {
-            state.loading = action.payload
+        setLoading(state) {
+            state.loading = true
+            state.error = null
         },
         setError(state, action) {
             state.error = action.payload
+            state.loading = false
         },
+        clearResults(state){
+            state.results = []
+        }
 
     }
 })
 
-export const { setQuery, setActiveTabs, setResults, setLoading, setError } = searchSlice.actions
+export const { setQuery, setActiveTabs, setResults, setLoading, setError, clearResults } = searchSlice.actions
 export default searchSlice.reducer
