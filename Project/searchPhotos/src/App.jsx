cc38965e920101095350847ea052fc99a0d60f84
ez-sourav <souravb2003@gmail.com>
2026-01-27 18:45:04 +0530

    commit

--- Project/searchPhotos/src/App.jsx
+++ Project/searchPhotos/src/App.jsx
@@ -1,37 +1,9 @@
-import { fetchGIF, fetchPhotos, fetchVideos } from "./api/mediaApi";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div className="bg-gray-950 h-screen w-full text-white">
-      <button
-        className="bg-amber-500 px-5 py-3 mr-5"
-        onClick={async () => {
-          const data = await fetchPhotos("dog");
-          console.log(data.results);
-        }}
-      >
-        Get Photos
-      </button>
-
-      <button
-        className="bg-amber-500 px-5 py-3 mr-5"
-        onClick={async () => {
-          const data = await fetchVideos("dog");
-          console.log(data.videos);
-        }}
-      >
-        Get Videos
-      </button>
-
-      <button
-        className="bg-amber-500 px-5 py-3 mr-5"
-        onClick={async () => {
-          const data = await fetchGIF("dog");
-          console.log(data.data);
-        }}
-      >
-        Get GIFs
-      </button>
+      <SearchBar/>
     </div>
   );
 };
