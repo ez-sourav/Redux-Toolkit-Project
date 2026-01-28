144e24b42149110975f185dd63072509d37fd16e
ez-sourav <souravb2003@gmail.com>
2026-01-28 20:42:49 +0530

    add buttons

---
+++ Project/searchPhotos/src/components/Tabs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveTabs } from "../redux/features/searchSlice";
+
+const Tabs = () => {
+  const tabs = ["photos", "videos", "GIF"];
+  const dispatch = useDispatch();
+  const activeTab = useSelector((state)=>state.search.activeTab)
+  return (
+    <div className="flex gap-5 p-10">
+      {tabs.map((elem, idx) => {
+        return (
+          <button
+            className={`${(activeTab == elem ? 'bg-blue-800':'bg-sky-600')} font-semibold capitalize cursor-pointer active:scale-95 px-6 py-2 rounded-md`}
+            onClick={()=>{
+                dispatch(setActiveTabs(elem))
+            }}
+            key={idx}
+          >
+            {elem}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Tabs;
