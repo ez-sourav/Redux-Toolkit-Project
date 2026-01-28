0eab2f758c722dc3db8ba696b8e42af97c247a94
ez-sourav <souravb2003@gmail.com>
2026-01-28 21:43:29 +0530

    mapping all results

--- Project/searchPhotos/src/components/ResultGrid.jsx
+++ Project/searchPhotos/src/components/ResultGrid.jsx
@@ -1,29 +1,57 @@
-import { useDispatch, useSelector } from 'react-redux'
-import {fetchPhotos,fetchVideos,fetchGIF} from '../api/mediaApi'
-import {setError,setLoading,setQuery,setResults} from '../redux/features/searchSlice'
-import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPhotos, fetchVideos, fetchGIF } from "../api/mediaApi";
+import {
+  setError,
+  setLoading,
+  setQuery,
+  setResults,
+} from "../redux/features/searchSlice";
+import { useEffect } from "react";
 
 const ResultGrid = () => {
-    const {query,activeTab,results,loadinger,error} = useSelector((store)=>store.search)
-    const getData = async ()=>{
-        let data;
-        if(activeTab == 'photos'){
-             data = await fetchPhotos(query); 
-        }
-        if(activeTab == 'videos'){
-             data = await fetchVideos(query); 
-        }
-        if(activeTab == 'gif'){
-             data = await fetchGIF(query); 
-        }
-    }
+  const { query, activeTab, results, loadinger, error } = useSelector(
+    (store) => store.search,
+  );
 
-    
-  return (
-    <div>
-        <button onClick={getData}>Get Data</button>
-    </div>
-  )
-}
+  useEffect(() => {
+    const getData = async () => {
+      let data;
+      if (activeTab == "photos") {
+        let response = await fetchPhotos(query);
+        data = response.results.map((item) => ({
+          id: item.id,
+          type: "photo",
+          title: item.alt_description,
+          thumbnail: item.urls.small,
+          src: item.urls.full,
+        }));
+      }
+      if (activeTab == "videos") {
+        let response = await fetchVideos(query);
+        data = response.videos.map((item) => ({
+          id: item.id,
+          type: "video",
+          title: item.user.name || "video",
+          thumbnail: item.image,
+          src: item.video_files[3].link,
+        }));
+      }
+      if (activeTab == "gif") {
+        let response = await fetchGIF(query);
+        data = response.data.map((item) => ({
+          id: item.id,
+          type: "gif",
+          title: item.title,
+          thumbnail: item.images.fixed_height_small_still.url,
+          src: item.images.original.url,
+        }));
+      }
+      console.log(data);
+    };
 
-export default ResultGrid
+    getData();
+  }, [query, activeTab]);
+  return <div></div>;
+};
+
+export default ResultGrid;
