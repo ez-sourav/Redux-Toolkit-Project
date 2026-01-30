9bb6398dd9ef6c322dde6cd065390ece2879bbd2
ez-sourav <souravb2003@gmail.com>
2026-01-30 19:04:50 +0530

    add home and collection page

---
+++ Project/searchPhotos/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+import Tabs from '../components/Tabs'
+import ResultGrid from '../components/ResultGrid'
+
+const HomePage = () => {
+  return (
+    <div>
+        <SearchBar/>
+      <Tabs/>
+      <ResultGrid/>
+    </div>
+  )
+}
+
+export default HomePage
