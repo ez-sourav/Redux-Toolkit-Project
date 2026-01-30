b475363b1f249faf7a185cae253ce4292540268a
ez-sourav <souravb2003@gmail.com>
2026-01-30 18:43:55 +0530

    change video result 15 to 20

--- Project/searchPhotos/src/api/mediaApi.js
+++ Project/searchPhotos/src/api/mediaApi.js
@@ -12,7 +12,7 @@ export async function fetchPhotos(query, page = 1, per_page = 20) {
     return res.data
 }
 
-export async function fetchVideos(query, per_page = 15) {
+export async function fetchVideos(query, per_page = 20) {
     const res = await axios.get('https://api.pexels.com/videos/search', {
         params: { query, per_page },
         headers: { Authorization: PEXELS_KEY }
