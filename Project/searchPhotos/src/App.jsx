6f9cafaa8696e1743e0cdce3c23f337ea3f8f9c7
ez-sourav <souravb2003@gmail.com>
2026-01-30 19:30:33 +0530

    commit

--- Project/searchPhotos/src/App.jsx
+++ Project/searchPhotos/src/App.jsx
@@ -1,13 +1,15 @@
-import ResultGrid from "./components/ResultGrid";
-import SearchBar from "./components/SearchBar";
-import Tabs from "./components/Tabs";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CollectionPage from "./pages/CollectionPage";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-950 w-full text-white">
-      <SearchBar/>
-      <Tabs/>
-      <ResultGrid/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/collection" element={<CollectionPage/>}/>
+      </Routes>
+      
     </div>
   );
 };
