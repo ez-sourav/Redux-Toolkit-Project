0e1011b96e53f019f8fe61384de93f7d04d6623c
ez-sourav <souravb2003@gmail.com>
2026-02-01 19:20:33 +0530

    add react dom

--- Project/searchPhotos/src/App.jsx
+++ Project/searchPhotos/src/App.jsx
@@ -1,15 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CollectionPage from "./pages/CollectionPage";
+import NavBar from "./components/NavBar";
+import { ToastContainer,Zoom } from "react-toastify";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-950 w-full text-white">
+    <div className="min-h-screen w-full bg-linear-to-br from-[#020617] via-[#020617] to-[#0f172a] text-white">
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/collection" element={<CollectionPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/collection" element={<CollectionPage />} />
       </Routes>
-      
+
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        limit={1}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="dark"
+        transition={Zoom}
+      />
     </div>
   );
 };
