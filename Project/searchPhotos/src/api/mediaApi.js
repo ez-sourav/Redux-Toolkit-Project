7be83bdcd3ae504b94c28a0cdd46feb67d538ce2
ez-sourav <souravb2003@gmail.com>
2026-02-05 19:59:49 +0530

    add page number in videos

--- Project/searchPhotos/src/api/mediaApi.js
+++ Project/searchPhotos/src/api/mediaApi.js
@@ -12,9 +12,9 @@ export async function fetchPhotos(query, page = 1, per_page = 20) {
     return res.data
 }
 
-export async function fetchVideos(query, per_page = 20) {
+export async function fetchVideos(query,page =1, per_page = 20) {
     const res = await axios.get('https://api.pexels.com/videos/search', {
-        params: { query, per_page },
+        params: { query,page, per_page },
         headers: { Authorization: PEXELS_KEY }
     })
     return res.data
