44870fdfd2906a8e0e3683154098aefda80bde7a
ez-sourav <souravb2003@gmail.com>
2026-02-05 20:00:44 +0530

    modify UI

--- Project/searchPhotos/src/pages/CollectionPage.jsx
+++ Project/searchPhotos/src/pages/CollectionPage.jsx
@@ -16,32 +16,35 @@ const CollectionPage = () => {
   return (
     <div className="px-4 sm:px-6 py-6">
       {collection.length > 0 ? (
-        <div className="max-w-7xl mx-auto mb-6 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
-          <h2 className="text-xl sm:text-2xl font-medium text-white">
-            Your Collection
-          </h2>
+  <div className="max-w-7xl mx-auto mb-6 flex items-center justify-between">
+    <h2 className="text-lg sm:text-2xl font-medium text-white">
+      Your Collection
+    </h2>
 
-          <button
-            onClick={clearAll}
-            className="
-    flex items-center gap-2
-    px-4 sm:px-5 py-2
-    rounded-md
-    text-sm sm:text-base
-    font-medium
-    bg-red-500
-    text-white
-    hover:bg-white/20
-    transition
-    active:scale-95
-    self-start sm:self-auto
-  "
-          >
-            <Trash2 className="w-4 h-4 sm:w-5 sm:h-5" />
-            <span>Clear Collection</span>
-          </button>
-        </div>
-      ) : (
+    <button
+      onClick={clearAll}
+      className="
+        flex items-center justify-center gap-2
+        h-9 w-9 sm:h-auto sm:w-auto
+        sm:px-5 sm:py-2
+        rounded-md
+        text-sm sm:text-base
+        font-medium
+        bg-red-500
+        text-white
+        hover:bg-red-600
+        hover:cursor-pointer
+        transition
+        active:scale-95
+      "
+      title="Clear Collection"
+    >
+      <Trash2 className="w-4 h-4 sm:w-5 sm:h-5" />
+      <span className="hidden sm:inline">Clear Collection</span>
+    </button>
+  </div>
+) : (
+
         <div className="flex flex-col items-center justify-center py-24 text-center">
           <Inbox className="w-12 h-12 sm:w-16 sm:h-16 text-white/20 mb-4" />
           <h2 className="text-2xl sm:text-4xl font-medium text-white/50">
