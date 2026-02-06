20f9c87cb9ebfb97503d90f55273e08abc510ba6
ez-sourav <souravb2003@gmail.com>
2026-02-06 19:13:21 +0530

    add pages

--- Project/searchPhotos/src/redux/features/searchSlice.js
+++ Project/searchPhotos/src/redux/features/searchSlice.js
@@ -8,18 +8,29 @@ const searchSlice = createSlice({
     results: [],
     loading: false,
     error: null,
+    page: 1,
+    maxPages: 10,
   },
   reducers: {
     setQuery(state, action) {
       state.query = action.payload;
+      state.page = 1; 
     },
+
     setActiveTabs(state, action) {
       state.activeTab = action.payload;
+      state.page = 1; 
     },
+
+    setPage(state, action) {
+      state.page = action.payload;
+    },
+
     setResults(state, action) {
       state.results = action.payload;
       state.loading = false;
     },
+
     setLoading(state, action) {
       state.loading = action.payload;
       if (action.payload) {
@@ -27,10 +38,12 @@ const searchSlice = createSlice({
         state.error = null;
       }
     },
+
     setError(state, action) {
       state.error = action.payload;
       state.loading = false;
     },
+
     clearResults(state) {
       state.results = [];
     },
@@ -44,6 +57,7 @@ export const {
   setLoading,
   setError,
   clearResults,
+  setPage,
 } = searchSlice.actions;
 
 export default searchSlice.reducer;
